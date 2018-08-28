66e1e0bd9c3994d49f9e2da73fbaf304f587fc0e
VadLusk <AdLusk@gmail.com>
2018-08-28 17:41:10 -0600

    Add routes with funcs that don't exist yet.

--- app.go
+++ app.go
@@ -9,5 +9,14 @@ import (
 
 func main() {
   router := mux.NewRouter()
+  router.HandleFunc("/api/v1/foods", CreateFood).Methods("POST")
+  router.HandleFunc("/api/v1/foods", GetFoods).Methods("GET")
+  router.HandleFunc("/api/v1/foods/{id}", GetFood).Methods("GET")
+  router.HandleFunc("/api/v1/foods/{id}", UpdateFood).Methods("PUT")
+  router.HandleFunc("/api/v1/foods/{id}", DeleteFood).Methods("DELETE")
+  router.HandleFunc("/api/v1/meals", GetMeals).Methods("GET")
+  router.HandleFunc("/api/v1/meals/{meal_id}/foods", GetMeal).Methods("GET")
+  router.HandleFunc("/api/v1/meals/{meal_id}/foods/{id}", CreateMealFood).Methods("POST")
+  router.HandleFunc("/api/v1/meals/{meal_id}/foods/{id}", DeleteMealFood).Methods("DELETE")
   log.Fatal(http.ListenAndServe(":3000", router))
 }
