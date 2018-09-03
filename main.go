92e2dada9a1f35741cfee7351dd0f5d5c9930454
VadLusk <AdLusk@gmail.com>
2018-09-03 17:54:04 -0600

    Delete commented db initialize func.

--- main.go
+++ main.go
@@ -77,21 +77,6 @@ func main() {
   log.Fatal(http.ListenAndServe(":"+port, handler))
 }
 
-// func initializeDB(host string, dbport int, user string, dbname string) sql.DB {
-//   psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
-//     host, dbport, user, dbname)
-//   db, err := sql.Open("postgres", psqlInfo)
-//   if err != nil {
-//     panic(err)
-//   }
-//   err = db.Ping()
-//   if err != nil {
-//     panic(err)
-//   }
-//   fmt.Println("Successfully connected to database!")
-//   return *db
-// }
-
 func CreateFood(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
   var f FoodStruct
