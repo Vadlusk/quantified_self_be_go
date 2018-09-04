f8e28e86b5680b41b3a5099f06c39edecf3bb7c1
VadLusk <AdLusk@gmail.com>
2018-09-03 20:04:16 -0600

    Move InitRoutes to own file. Delete old placement.

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 
   "github.com/vadlusk/quantified_self_be_go/db"
   "github.com/rs/cors"
-  "github.com/gorilla/mux"
   _ "github.com/lib/pq"
 )
 
@@ -66,20 +65,6 @@ func main() {
   }
   fmt.Println("Listening on port "+port)
   log.Fatal(http.ListenAndServe(":"+port, handler))
-}
-
-func InitRoutes() *mux.Router {
-  r := mux.NewRouter()
-  r.HandleFunc("/api/v1/foods/", CreateFood).Methods("POST")
-  r.HandleFunc("/api/v1/foods/", GetFoods).Methods("GET")
-  r.HandleFunc("/api/v1/foods/{id}", GetFood).Methods("GET")
-  r.HandleFunc("/api/v1/foods/{id}", UpdateFood).Methods("PUT")
-  r.HandleFunc("/api/v1/foods/{id}", DeleteFood).Methods("DELETE")
-  r.HandleFunc("/api/v1/meals/", GetMeals).Methods("GET")
-  r.HandleFunc("/api/v1/meals/{meal_id}/foods", GetMeal).Methods("GET")
-  r.HandleFunc("/api/v1/meals/{meal_id}/foods/{id}", CreateMealFood).Methods("POST")
-  r.HandleFunc("/api/v1/meals/{meal_id}/foods/{id}", DeleteMealFood).Methods("DELETE")
-  return r
 }
 
 func CreateFood(w http.ResponseWriter, r *http.Request) {
