db247d3b3fb06d0543e67b4cd5810ac74c1b4ef3
VadLusk <AdLusk@gmail.com>
2018-09-03 19:58:46 -0600

    Delete commented old routes.

--- main.go
+++ main.go
@@ -59,16 +59,6 @@ func main() {
     Debug: true,
   })
   r := InitRoutes()
-  // r := mux.NewRouter()
-  // r.HandleFunc("/api/v1/foods/", CreateFood).Methods("POST")
-  // r.HandleFunc("/api/v1/foods/", GetFoods).Methods("GET")
-  // r.HandleFunc("/api/v1/foods/{id}", GetFood).Methods("GET")
-  // r.HandleFunc("/api/v1/foods/{id}", UpdateFood).Methods("PUT")
-  // r.HandleFunc("/api/v1/foods/{id}", DeleteFood).Methods("DELETE")
-  // r.HandleFunc("/api/v1/meals/", GetMeals).Methods("GET")
-  // r.HandleFunc("/api/v1/meals/{meal_id}/foods", GetMeal).Methods("GET")
-  // r.HandleFunc("/api/v1/meals/{meal_id}/foods/{id}", CreateMealFood).Methods("POST")
-  // r.HandleFunc("/api/v1/meals/{meal_id}/foods/{id}", DeleteMealFood).Methods("DELETE")
   handler := c.Handler(r)
   port := os.Getenv("PORT")
   if port == "" {
