895a4517b4708f75959829786a4204c8bdca5556
VadLusk <AdLusk@gmail.com>
2018-09-09 19:08:58 -0600

    Add content to meals index.

--- controllers/api/v1/meals/mealsController.go
+++ controllers/api/v1/meals/mealsController.go
@@ -1,11 +1,16 @@
 package meals
 
 import (
+  "encoding/json"
   "net/http"
+
+  "github.com/vadlusk/quantified_self_be_go/models/Meal"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
+  meals := Meal.all()
+  json.NewEncoder(w).Encode(meals)
 }
 
 func Show(w http.ResponseWriter, r *http.Request) {
