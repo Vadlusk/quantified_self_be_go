c96f0b06f5da5e1f4582f318b354e2c18b562ec7
VadLusk <AdLusk@gmail.com>
2018-09-15 16:38:53 -0600

    Add content and libraries to mealFood Create.

--- controllers/api/v1/meals/foods/mealFoodsController.go
+++ controllers/api/v1/meals/foods/mealFoodsController.go
@@ -1,11 +1,18 @@
 package foods
 
 import (
+  "encoding/json"
   "net/http"
+
+  "github.com/vadlusk/quantified_self_be_go/models/MealFood"
+  "github.com/gorilla/mux"
 )
 
 func Create(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
+  params   := mux.Vars(r)
+  mealFood := MealFood.Create(params["id"], params["meal_id"])
+  json.NewEncoder(w).Encode(mealFood)
 }
 
 func Destroy(w http.ResponseWriter, r *http.Request) {
