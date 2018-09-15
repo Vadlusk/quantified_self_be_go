af2c6c56a49065bd4e3fc9baa0be1fae558ca722
VadLusk <AdLusk@gmail.com>
2018-09-15 17:46:39 -0600

    Add meal food destroy to model.

--- models/MealFood/mealFoodModel.go
+++ models/MealFood/mealFoodModel.go
@@ -13,8 +13,17 @@ func Create(foodId, mealId string) string {
   query := "INSERT INTO meal_foods (food_id, meal_id) VALUES ($1, $2) RETURNING id"
   err := db.Instance().QueryRow(query, foodId, mealId).Scan(&id)
   if err != nil { panic(err) }
-  message := fmt.Sprintf("Successfully added %v to %v", foodName(foodId), mealName(mealId))
-  return message
+  msg := fmt.Sprintf("Successfully added %v to %v", foodName(foodId), mealName(mealId))
+  return msg
+}
+
+func Destroy(foodId, mealId string) string {
+  var id string
+  query := "DELETE FROM meal_foods WHERE meal_id=$1 AND food_id=$2 RETURNING id"
+  err := db.Instance().QueryRow(query, mealId, foodId).Scan(&id)
+  if err != nil { panic(err) }
+  msg := fmt.Sprintf("Successfully removed %v from %v", foodName(foodId), mealName(mealId))
+  return msg
 }
 
 func foodName(foodId string) string {
