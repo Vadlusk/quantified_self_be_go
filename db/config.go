4c284e7037e0dff080478b3af5e2bac03b06d763
VadLusk <AdLusk@gmail.com>
2018-09-16 19:38:48 -0600

    Add password to connection string.

--- db/config.go
+++ db/config.go
@@ -21,8 +21,8 @@ func Instance() *sql.DB {
 
 func InitDB() sql.DB {
   psqlInfo := fmt.Sprintf(
-    "host=%s port=%s user=%s dbname=%s sslmode=disable",
-    env.Host(), env.DbPort(), env.User(), env.DbName())
+    "host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
+    env.Host(), env.DbPort(), env.User(), env.DbName(), env.Password())
   db, err := sql.Open("postgres", psqlInfo)
   if err != nil { panic(err) }
   err = db.Ping()
